ddb1c95c6bb70179ae2110d722ea8856714c6fd9
TrustCode01 <trustcharingani@gmail.com>
2024-09-29 08:27:54 -0400

    adding functions to track items added to cart with redux

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -285,6 +285,8 @@ const handlePlantsClick = (e) => {
             <div className="product-card" key={plantIndex}>
                 <img className="product-image" src={plant.image} alt={plant.name} />
                 <div className="product-title">{plant.name}</div>
+                <p className="product-description">{plant.description}</p>
+                <p className="product-cost">{plant.cost}</p>
                 {/*Similarly like the above plant.name show other details like description and cost*/}
                 <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
             </div>
